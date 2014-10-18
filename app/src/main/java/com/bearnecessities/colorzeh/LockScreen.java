cf719797d6dd05055bb2dedd6c8ba9dd6868708e
stammatime <stammer.3@osu.edu>
2014-10-17 21:26:11 -0400

    Added button functionality

--- app/src/main/java/com/bearnecessities/colorzeh/LockScreen.java
+++ app/src/main/java/com/bearnecessities/colorzeh/LockScreen.java
@@ -1,18 +1,105 @@
 package com.bearnecessities.colorzeh;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.PaintDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
-public class LockScreen extends ActionBarActivity {
+public class LockScreen extends Activity implements View.OnClickListener{
+
+    ImageButton tl, tm, tr, ml, mm, mr, bl, bm, br;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
+        initializeButtons();
     }
+
+    private void initializeButtons()
+    {
+        tl = (ImageButton)  findViewById(R.id.top_left);
+        tm = (ImageButton)  findViewById(R.id.top_mid);
+        tr = (ImageButton)  findViewById(R.id.top_right);
+        ml = (ImageButton)  findViewById(R.id.mid_left);
+        mm = (ImageButton)  findViewById(R.id.mid_mid);
+        mr = (ImageButton)  findViewById(R.id.mid_right);
+        bl = (ImageButton)  findViewById(R.id.bot_left);
+        bm = (ImageButton)  findViewById(R.id.bot_mid);
+        br = (ImageButton)  findViewById(R.id.bot_right);
+
+        tl.setOnClickListener(this);
+        tm.setOnClickListener(this);
+        tr.setOnClickListener(this);
+        ml.setOnClickListener(this);
+        mm.setOnClickListener(this);
+        mr.setOnClickListener(this);
+        bl.setOnClickListener(this);
+        bm.setOnClickListener(this);
+        br.setOnClickListener(this);
+
+        /*
+              0 1 2
+              _ _ _
+         0   |_|_|_|
+         1   |_|_|_|
+         2   |_|_|_|
+
+        */
+    }
+
+    @Override
+    public void onClick(View v) {
+        int locationX;
+        int locationY;
+
+        switch(v.getId()) {
+            case R.id.top_left:
+                locationX = 0;
+                locationY = 0;
+                break;
+            case R.id.top_mid:
+                locationX = 1;
+                locationY = 0;
+                break;
+            case R.id.top_right:
+                locationX = 2;
+                locationY = 0;
+                break;
+            case R.id.mid_left:
+                locationX = 0;
+                locationY = 1;
+                break;
+
+            case R.id.mid_mid:
+                locationX = 1;
+                locationY = 1;
+                break;
+            case R.id.mid_right:
+                locationX = 2;
+                locationY = 1;
+                break;
+
+            case R.id.bot_left:
+                locationX = 0;
+                locationY = 2;
+                break;
+            case R.id.bot_mid:
+                locationX = 1;
+                locationY = 1;
+                break;
+
+            case R.id.bot_right:
+                locationX = 2;
+                locationY = 2;
+                break;
+        }}
 
 
     @Override
@@ -33,4 +120,6 @@ public class LockScreen extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
